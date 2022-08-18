a2313f19c39215665756c758fec19c22ba35c514
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-08-19 01:48:36 +0800

    core(event): first implementation

---
+++ code/core/Event.cs
@@ -0,0 +1,48 @@
+/// <summary>
+/// part of the gm0 (w.i.p name) gamemode
+/// - lotuspar, 2022 (github.com/lotuspar)
+/// </summary>
+
+namespace gm0;
+
+/// <summary>
+/// Data for a single in-game event. Can be used as a network packet
+/// var1 & var2 are multi-purpose variables based on the value of action
+/// </summary>
+public struct GameEvent {
+    public GameEvent(uint uid, uint action, uint var1 = 0, uint var2 = 0) {
+        this.uid = uid;
+        this.action = action;
+        this.var1 = var1;
+        this.var2 = var2;
+    }
+
+    public readonly uint uid;
+    public readonly uint action;
+    public uint var1;
+    public uint var2;
+}
+
+/// <summary>
+/// Registry of all known events
+/// </summary>
+public enum GameEventAction : uint {
+    INVALID = 0,
+    ACK = 1
+}
+
+/// <summary>
+/// Class to quickly create event for specific action
+/// </summary>
+public class GameEventCreator {
+    /// <summary>
+    /// Acknowledge processing of a received event
+    /// </summary>
+    /// <param name="uid">New event UID</param>
+    /// <param name="acknowledgedEventUid">Received event UID</param>
+    /// <param name="status">Status code to provide back (0 always OK)</param>
+    /// <returns>New event</returns>
+	public static GameEvent Acknowledgement(uint uid, uint acknowledgedEventUid = 0, uint status = 0) {
+        return new GameEvent(uid, (uint) GameEventAction.ACK, acknowledgedEventUid, status);
+    }
+}
