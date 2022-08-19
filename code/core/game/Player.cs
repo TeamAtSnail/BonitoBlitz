fa33fcb702fa1bb54ec5ce2b8002ee0051cd4e7d
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-08-20 02:15:25 +0800

    core(game): implement `Player`
    just basic player class based on sbox-pool Player

---
+++ code/core/game/Player.cs
@@ -0,0 +1,32 @@
+/// <summary>
+/// part of the gm0 (w.i.p name) gamemode
+/// - lotuspar, 2022 (github.com/lotuspar)
+/// semi based on the Player class for sbox-pool 
+///   -> (https://github.com/Facepunch/sbox-pool/blob/master/code/player/Player.cs)
+/// </summary>
+namespace gm0;
+using Sandbox;
+
+/// <summary>
+/// Player class for the gm0 gamemode
+/// </summary>
+class Player : Entity {
+    /// <summary>
+	/// Player camera attribute
+	/// Uses EventCamera which acts based on server requests
+	/// </summary>
+    public EventCamera Camera {
+        get => Components.Get<EventCamera>();
+		set {
+			var current = Camera;
+			if ( current == value ) return;
+
+			Components.RemoveAny<EventCamera>();
+			Components.Add( value );
+		}
+    }
+
+    public Player() {
+		Camera = new EventCamera();
+	}
+}
