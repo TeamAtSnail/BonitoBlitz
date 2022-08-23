8f203148183e904212f8acd7a3a8298443230476
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-08-23 17:36:52 +0800

    core(session): implement `ClientSession` TX

---
+++ code/core/Session.Client.EventTx.cs
@@ -0,0 +1,20 @@
+/*
+ * part of the gm0 (w.i.p name) gamemode
+ * - lotuspar, 2022 (github.com/lotuspar)
+ */
+namespace gm0;
+
+/// <summary>
+/// Data for a single client-side session
+/// </summary>
+public static partial class ClientSession
+{
+	/// <summary>
+	/// Send game event to server
+	/// </summary>
+	/// <param name="evt">Event to send</param>
+	public static void SendEvent( GameEvent evt )
+	{
+		ServerSession.HandleEventCcmdShim( evt );
+	}
+}
