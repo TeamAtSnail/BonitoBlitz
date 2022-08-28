21dbc9296f4e0bd07ee01b4f2115b3d8e53db927
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-08-28 21:43:04 +0800

    game(entity): implement `StartArea`
    experimental

---
+++ code/entities/StartArea.cs
@@ -0,0 +1,16 @@
+/*
+ * part of the gm0 (w.i.p name) gamemode
+ * - lotuspar, 2022 (github.com/lotuspar)
+ */
+namespace gm0;
+using Sandbox;
+using SandboxEditor;
+
+/// <summary>
+/// Starting area for players and initial tutorial
+/// </summary>
+[Library( "gm0_start_area" ), HammerEntity, Solid]
+[Title( "Starting Area" ), Category( "Gameplay" ), Icon( "place" )]
+[Description( "Start / tutorial area" )]
+public class StartArea : Entity
+{ }
