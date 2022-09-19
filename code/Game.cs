f670fe73e2f8e11d73dfe82d31c4620ce6977aae
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-09-19 14:29:06 +0800

    game: checks and v2 of prev. commit

--- code/Game.cs
+++ code/Game.cs
@@ -4,13 +4,52 @@
  * - lotuspar, 2022 (github.com/lotuspar)
  */
 namespace gm0;
+using System;
 using Sandbox;
 
 public partial class Gamemode0 : libgm0.Game
 {
+	[Net]
+	private StartArea StartArea { get; set; } = null;
+
+	[Net]
+	private MapCamera StartCamera { get; set; } = null;
 
 	public Gamemode0() : base( libgm0.GameData.LoadActiveGame() )
+	{ }
+
+	public override void PostLevelLoaded()
 	{
+		/* Pre game checks */
+		// Entity gathering
+		foreach ( var entity in Entity.All )
+		{
+			// Make sure there's only 1 StartArea...
+			// (also get the StartArea)
+			if ( entity is StartArea startArea )
+			{
+				if ( StartArea != null )
+					throw new libgm0.error.GameMapException( "Map has multiple StartAreas!" );
+				StartArea = startArea;
+			}
+
+			// Make sure there's only 1 starting MapCamera...
+			// (also get the MapCamera)
+			if ( entity is MapCamera mapCamera && mapCamera.Name == "StartCamera" )
+			{
+				if ( StartCamera != null )
+					throw new libgm0.error.GameMapException( "Map has multiple cameras called StartCamera!" );
+				StartCamera = mapCamera;
+			}
+		}
+
+		// Make sure we found a StartArea
+		if ( Host.IsServer && StartArea == null )
+			throw new libgm0.error.GameMapException( "Map has no StartArea!" );
+
+		// Make sure there's a camera with the name StartCamera
+		if ( Host.IsServer && StartCamera == null )
+			throw new libgm0.error.GameMapException( "Map has no camera called StartCamera!" );
 	}
 
 	public override libgm0.Pawn PlayerJoined( Client cl, libgm0.PlayerData playerData )
@@ -23,14 +62,23 @@ public partial class Gamemode0 : libgm0.Game
 
 	public override void OnAllPlayersConnected()
 	{
-		// todo: why isn't this working?
-		BroadcastCamera( "mc_overview" );
-
-		foreach ( var entity in Entity.All )
+		uint distance = 64;
+		float delta = 360 / Data.Players.Count * (MathF.PI / 180);
+		for ( int i = 0; i < Data.Players.Count; i++ )
 		{
-			if ( entity is StartArea area )
+			if ( Data.Players[i].Pawn is BoardPawn player )
 			{
-				MovePlayersToStartArea( area );
+				// Move player to their starting spot
+				player.Position = new( 0, 0, StartArea.Position.z )
+				{
+					x = StartArea.Position.x + MathF.Sin( delta * i ) * distance,
+					y = StartArea.Position.y + MathF.Cos( delta * i ) * distance
+				};
+
+				// Give player new camera
+				var camera = new PointCamera();
+				camera.SetToMapCamera( StartCamera );
+				player.Camera = camera;
 			}
 		}
 	}
