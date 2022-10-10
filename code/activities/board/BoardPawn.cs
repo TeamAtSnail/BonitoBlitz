df810afa54cc728263dfe160b1edf14ec78e1c8e
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-10-10 15:51:16 +0800

    debug/activity/board: update `BoardPawn`
    impl `BoardCameraMode` support
    fix client NRE

--- code/activities/board/BoardPawn.cs
+++ code/activities/board/BoardPawn.cs
@@ -12,10 +12,39 @@ public partial class BoardPawn : AnimatedEntity
 {
 	[Net] public libblitz.Player Player { get; private set; }
 
+	public BoardCameraMode Camera
+	{
+		get => Components.Get<BoardCameraMode>();
+		set
+		{
+			if ( Camera == value ) return;
+			Components.RemoveAny<BoardCameraMode>();
+			Components.Add( value );
+		}
+	}
+
 	public BoardPawn( libblitz.Player player ) : base()
 	{
 		Player = player;
+
+		Camera = new BoardCameraMode();
+
+		if ( Player.Client != null )
+		{
+			// Load player clothing
+			ClothingContainer clothing = new();
+			clothing.LoadFromClient( Player.Client );
+
+			// Dress player
+			clothing.DressEntity( this );
+		}
 	}
 
-	public BoardPawn() : base() {}
+	public BoardPawn() : base() { }
+
+	public override void Spawn()
+	{
+		// Load citizen model
+		SetModel( "models/citizen/citizen.vmdl" );
+	}
 }
