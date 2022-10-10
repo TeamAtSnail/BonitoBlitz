ab51ccebf7b16168563005ccecfabe5b536bcf58
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-10-10 15:56:10 +0800

    sharedui(debug): add ActivityInfo label

--- code/sharedui/Debug/Debug.cs
+++ code/sharedui/Debug/Debug.cs
@@ -13,6 +13,19 @@ public partial class Debug : Panel
 	public static libblitz.Game CurrentGame => Sandbox.Game.Current as libblitz.Game;
 	public string GameInfo => $"Game {CurrentGame.Uid}/{CurrentGame.DisplayName}";
 	public string GamePlayersInfo => $"Player count {CurrentGame.Players.Count}";
+	public string ActivityInfo
+	{
+		get
+		{
+			if (CurrentGame == null)
+				return "No game active";
+			if (CurrentGame.Activities == null)
+				return "Activities list broken";
+			if (CurrentGame.Activity == null)
+				return "No activity active";
+			return CurrentGame.Activity.GetType().Name;
+		}
+	}
 
 	public Debug() { }
 }
